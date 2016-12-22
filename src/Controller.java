04767c0e9e793f0b25e3f5a12c3cf4f77828dfa5
Connor Borzello <connorborz@gmail.com>
2016-12-21 23:47:30 -0600

    Created Main GUI

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,12 @@
+import java.awt.*;
+
 /**
  * Created by Connor Borzello on 12/21/2016.
  * Description:
  */
 public class Controller {
+    public static void main(String[] args) {
+        new View();
+
+    }
 }
